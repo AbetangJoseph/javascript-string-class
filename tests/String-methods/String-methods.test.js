d86e6fc75f5eef063a64523ef2c1e175054ae611
Abetang Joseph <josephabetang@gmail.com>
2019-05-08 00:47:21 +0100

    add test toUpper method

--- tests/String-methods/String-methods.test.js
+++ tests/String-methods/String-methods.test.js
@@ -1,15 +1,22 @@
 var String = require('../../src/String-methods/String-methods');
 
 describe('STRING METHODS', function() {
-  describe('hasVowels', function() {
-    it('should return false if the string contains vowels', function() {
-      var string1 = 'rhythm crypt';
-      expect(string1.hasVowels()).toBeFalsy();
-    });
+	describe('hasVowels', function() {
+		it('should return false if the string contains vowels', function() {
+			var string1 = 'rhythm crypt';
+			expect(string1.hasVowels()).toBeFalsy();
+		});
 
-    it('should return true if the string contains vowels', function() {
-      var string2 = 'vowel checker';
-      expect(string2.hasVowels()).toBeTruthy();
-    });
-  });
+		it('should return true if the string contains vowels', function() {
+			var string2 = 'vowel checker';
+			expect(string2.hasVowels()).toBeTruthy();
+		});
+	});
+
+	describe('toUpper', function() {
+		it('should return a string with all its characters in uppercase', function() {
+			var string = 'a capitalized sentence';
+			expect(string.toUpper()).toEqual('A CAPITALIZED SENTENCE');
+		});
+	});
 });
