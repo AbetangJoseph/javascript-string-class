1f9ec168e8b8b5903fd17c593beeca40d0377bda
Abetang Joseph <josephabetang@gmail.com>
2019-05-08 04:08:09 +0100

    fix tab indent

--- src/String-methods/String-methods.js
+++ src/String-methods/String-methods.js
@@ -2,52 +2,52 @@
 This method checks a string and returns true if it contains a vowel and false if it doesn't.
 */
 String.prototype.hasVowels = function() {
-	var regExPattern = /[aeiou]/gi;
-	return regExPattern.test(this);
+  var regExPattern = /[aeiou]/gi;
+  return regExPattern.test(this);
 };
 
 /* 
 This method checks a string for lower-case alphabet(s) and convert to upper-case
 */
 String.prototype.toUpper = function() {
-	var regExPattern = /[a-z]/g;
-	return this.replace(regExPattern, function(alphabet) {
-		return String.fromCharCode(alphabet.charCodeAt(0) - 32);
-	});
+  var regExPattern = /[a-z]/g;
+  return this.replace(regExPattern, function(alphabet) {
+    return String.fromCharCode(alphabet.charCodeAt(0) - 32);
+  });
 };
 
 /* 
 This method checks a string for upper-case alphabet(s) and convert to lower-case
 */
 String.prototype.toLower = function() {
-	var regExPattern = /[A-Z]/g;
-	return this.replace(regExPattern, function(alphabet) {
-		return String.fromCharCode(alphabet.charCodeAt(0) + 32);
-	});
+  var regExPattern = /[A-Z]/g;
+  return this.replace(regExPattern, function(alphabet) {
+    return String.fromCharCode(alphabet.charCodeAt(0) + 32);
+  });
 };
 
 /* 
 This method converts the first character(s) in a string to upper-case
 */
 String.prototype.ucFirst = function() {
-	var regExPattern = /(\b\w+\b)/g;
-	return this.replace(regExPattern, function(word) {
-		return word[0].toUpper() + word.substring(1);
-	});
+  var regExPattern = /(\b\w+\b)/g;
+  return this.replace(regExPattern, function(word) {
+    return word[0].toUpper() + word.substring(1);
+  });
 };
 
 /* 
 This method returns true if the string is a question
 */
 String.prototype.isQuestion = function() {
-	var regExPattern = /\w+\?$/gi;
-	return regExPattern.test(this);
+  var regExPattern = /\w+\?$/gi;
+  return regExPattern.test(this);
 };
 
 /* 
 This method returns a list of the words in the string, as an Array excluding numbers and special characters
 */
 String.prototype.word = function() {
-	var regExPattern = /\b([^\d.\W][a-z]*)\b/gi;
-	return this.match(regExPattern);
+  var regExPattern = /\b([^\d.\W][a-z]*)\b/gi;
+  return this.match(regExPattern);
 };
--- tests/String-methods/String-methods.test.js
+++ tests/String-methods/String-methods.test.js
@@ -1,56 +1,56 @@
 var String = require('../../src/String-methods/String-methods');
 
 describe('STRING METHODS', function() {
-	describe('hasVowels', function() {
-		it('should return false if the string contains vowels', function() {
-			var string1 = 'rhythm crypt';
-			expect(string1.hasVowels()).toBeFalsy();
-		});
+  describe('hasVowels', function() {
+    it('should return false if the string contains vowels', function() {
+      var string1 = 'rhythm crypt';
+      expect(string1.hasVowels()).toBeFalsy();
+    });
 
-		it('should return true if the string contains vowels', function() {
-			var string2 = 'vowel checker';
-			expect(string2.hasVowels()).toBeTruthy();
-		});
-	});
+    it('should return true if the string contains vowels', function() {
+      var string2 = 'vowel checker';
+      expect(string2.hasVowels()).toBeTruthy();
+    });
+  });
 
-	describe('toUpper', function() {
-		it('should return a string with all its characters in uppercase', function() {
-			var string = 'a capitalized sentence';
-			expect(string.toUpper()).toEqual('A CAPITALIZED SENTENCE');
-		});
-	});
+  describe('toUpper', function() {
+    it('should return a string with all its characters in uppercase', function() {
+      var string = 'a capitalized sentence';
+      expect(string.toUpper()).toEqual('A CAPITALIZED SENTENCE');
+    });
+  });
 
-	describe('toLower', function() {
-		it('should return a string with all its characters in lowercase', function() {
-			var string = 'A LOWER CASE SENTENCE';
-			expect(string.toLower()).toEqual('a lower case sentence');
-		});
-	});
+  describe('toLower', function() {
+    it('should return a string with all its characters in lowercase', function() {
+      var string = 'A LOWER CASE SENTENCE';
+      expect(string.toLower()).toEqual('a lower case sentence');
+    });
+  });
 
-	describe('ucFirst', function() {
-		it('should return a string with the first character capitalized', function() {
-			var string = 'i love javascript';
-			expect(string.ucFirst()).toEqual('I Love Javascript');
-		});
-	});
+  describe('ucFirst', function() {
+    it('should return a string with the first character capitalized', function() {
+      var string = 'i love javascript';
+      expect(string.ucFirst()).toEqual('I Love Javascript');
+    });
+  });
 
-	describe('isQuestion', function() {
-		it('should return true if the string is a question', function() {
-			var string = 'can you swim?';
-			expect(string.isQuestion()).toBeTruthy();
-		});
+  describe('isQuestion', function() {
+    it('should return true if the string is a question', function() {
+      var string = 'can you swim?';
+      expect(string.isQuestion()).toBeTruthy();
+    });
 
-		it('should return false if the string is not a question', function() {
-			var string = 'can you swim';
-			expect(string.isQuestion()).toBeFalsy();
-		});
-	});
+    it('should return false if the string is not a question', function() {
+      var string = 'can you swim';
+      expect(string.isQuestion()).toBeFalsy();
+    });
+  });
 
-	describe('word', function() {
-		it('should return a list of the words in the string, as an Array', function() {
-			var string = 'A software engineer at 25';
-			expect(string.word()).toContain('engineer');
-			expect(string.word()).not.toContain(25);
-		});
-	});
+  describe('word', function() {
+    it('should return a list of the words in the string, as an Array', function() {
+      var string = 'A software engineer at 25';
+      expect(string.word()).toContain('engineer');
+      expect(string.word()).not.toContain(25);
+    });
+  });
 });
