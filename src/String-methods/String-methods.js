7ce0e6f68b37a8a385bebbf9d81896b7ed5a120d
Abetang Joseph <josephabetang@gmail.com>
2019-05-08 01:36:41 +0100

    fix test failure for toUpper method

--- src/String-methods/String-methods.js
+++ src/String-methods/String-methods.js
@@ -2,6 +2,16 @@
 This method checks a string and returns true if it contains a vowel and false if it doesn't.
 */
 String.prototype.hasVowels = function() {
-  var regExPattern = /[aeiou]/;
-  return regExPattern.test(this);
+	var regExPattern = /[aeiou]/;
+	return regExPattern.test(this);
+};
+
+/* 
+This method checks a string for lower-case alphabet(s) and convert to upper-case
+*/
+String.prototype.toUpper = function() {
+	var regExPattern = /[a-z]/g;
+	return this.replace(regExPattern, function(alphabet) {
+		return String.fromCharCode(alphabet.charCodeAt(0) - 32);
+	});
 };
