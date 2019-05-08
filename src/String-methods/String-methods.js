20d1cb632f6e31007d0683fd8dc2e519ab636c43
Abetang Joseph <josephabetang@gmail.com>
2019-05-08 01:41:40 +0100

    update hasVowels method to contain gi regEx flags

--- src/String-methods/String-methods.js
+++ src/String-methods/String-methods.js
@@ -2,7 +2,7 @@
 This method checks a string and returns true if it contains a vowel and false if it doesn't.
 */
 String.prototype.hasVowels = function() {
-	var regExPattern = /[aeiou]/;
+	var regExPattern = /[aeiou]/gi;
 	return regExPattern.test(this);
 };
 
